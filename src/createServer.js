6edfa03e33b1c2958b96cb7711184085eb5b94d3
Haiana Reznichenko <gayanaalekseevna@gmail.com>
2026-03-10 19:25:09 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,79 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const publicDir = path.resolve(__dirname, '../public');
+    const requestUrl = req.url || '';
+
+    if (requestUrl === '/file') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Use /file/<path-to-file> to load files from public folder');
+
+      return;
+    }
+
+    if (!requestUrl.startsWith('/file/')) {
+      res.writeHead(400, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Bad request');
+
+      return;
+    }
+
+    const requestedPath = requestUrl.slice('/file/'.length);
+
+    if (requestedPath.includes('//')) {
+      res.writeHead(404, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('File not found');
+
+      return;
+    }
+
+    if (requestedPath.includes('..')) {
+      res.writeHead(400, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Bad request');
+
+      return;
+    }
+
+    const fileRelativePath =
+      requestedPath === '' ? 'index.html' : requestedPath;
+    const filePath = path.resolve(publicDir, fileRelativePath);
+
+    if (!filePath.startsWith(publicDir)) {
+      res.writeHead(400, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Bad request');
+
+      return;
+    }
+
+    fs.readFile(filePath, (error, data) => {
+      if (error) {
+        res.writeHead(404, {
+          'Content-Type': 'text/plain',
+        });
+        res.end('File not found');
+
+        return;
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
 }
 
 module.exports = {
